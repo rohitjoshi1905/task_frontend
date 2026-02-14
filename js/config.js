7b6b6978300a8bdf541bea938262adcc10a51b90
Rohit Joshi <rohitjoshi1905@gmail.com>
2026-02-14 18:00:49 +0530

    Update API URL to Render

---
+++ js/config.js
@@ -0,0 +1,13 @@
+// ==============================================
+// 1. FOR LOCALHOST (Running on same machine)
+// ==============================================
+const API_BASE_URL = "https://task-backend-2y43.onrender.com";
+// const API_BASE_URL = "http://localhost:8000";
+
+// ==============================================
+// 2. FOR WIFI / NETWORK (Phone or other laptop)
+// ==============================================
+// 1. Open CMD, type: ipconfig
+// 2. Copy "IPv4 Address" (e.g., 192.168.1.5)
+// 3. Uncomment below and paste your IP:
+// const API_BASE_URL = "http://192.168.1.X:8000";
